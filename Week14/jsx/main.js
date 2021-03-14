5bc4df67eb08064c70f09afbcaf68d3ed094abd8
Youwithouto <youwithouto.z@gmail.com>
2021-03-14 14:33:40 +1100

    feat: Add jsx

---
+++ Week14/jsx/main.js
@@ -0,0 +1,82 @@
+function createElement(type, attributes, ...children) {
+    let element;
+    if (typeof type === "string") {
+        element = new ElementWrapper(type);
+    } else {
+        element = new type;
+    }
+
+    for (let name in attributes) {
+        element.setAttribute(name, attributes[name]);
+    }
+
+    for (let child of children) {
+        if (typeof child === "string") {
+            child = new TextWrapper(child);
+        }
+        element.appendChild(child);
+    }
+
+    return element;
+}
+
+class ElementWrapper {
+    constructor(type) {
+        this.root = document.createElement(type);
+    }
+
+    setAttribute(name, value) {
+        this.root.setAttribute(name, value);
+    }
+
+    appendChild(child) {
+        child.mountTo(this.root);
+    }
+
+    mountTo(parent) {
+        parent.appendChild(this.root);
+    }
+}
+
+class TextWrapper {
+    constructor(content) {
+        this.root = document.createTextNode(content);
+    }
+
+    setAttribute(name, value) {
+        this.root.setAttribute(name, value);
+    }
+
+    appendChild(child) {
+        child.mountTo(this.root);
+    }
+
+    mountTo(parent) {
+        parent.appendChild(this.root);
+    }
+}
+
+class Div {
+    constructor() {
+        this.root = document.createElement("div");
+    }
+
+    setAttribute(name, value) {
+        this.root.setAttribute(name, value);
+    }
+
+    appendChild(child) {
+        child.mountTo(this.root);
+    }
+
+    mountTo(parent) {
+        parent.appendChild(this.root);
+    }
+}
+
+var a = <div id="a">
+    Hello, World!
+</div>;
+
+// document.body.appendChild(a);
+a.mountTo(document.body);

2538744dc4ecec04303599cda8c313c7d03197e1
Youwithouto <youwithouto.z@gmail.com>
2021-03-14 16:46:06 +1100

    feat: Add autoplay

--- Week14/jsx/main.js
+++ Week14/jsx/main.js
@@ -15,15 +15,35 @@ class Carousel extends Component {
     }
 
     render() {
-
-        console.log(this.attributes.src);
-
         this.root = document.createElement("div");
+
+        this.root.classList.add("carousel");
+
         for (let record of this.attributes.src) {
-            let child = document.createElement("img");
-            child.src = record;
+            let child = document.createElement("div");
+            child.style.backgroundImage = `url('${record}')`;
             this.root.appendChild(child);
         }
+
+        let currentIndex = 0;
+        setInterval(() => {
+            let children = this.root.children;
+            let nextIndex = (currentIndex + 1) % children.length;
+
+            let current = children[currentIndex];
+            let next = children[nextIndex];
+
+            next.style.transition = 'none';
+            next.style.transform = `translateX(${100 - nextIndex * 100}%)`;
+
+            setTimeout(() => {
+                next.style.transition = '';
+                current.style.transform = `translateX(${-100 - currentIndex * 100}%)`;
+                next.style.transform = `translateX(${- nextIndex * 100}%)`;
+                currentIndex = nextIndex;
+            }, 16);
+        }, 3000);
+
         return this.root;
     }
 

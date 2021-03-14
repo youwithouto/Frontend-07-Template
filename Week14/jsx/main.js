dc31535b684384e10639651772c4bf39f59de3d6
Youwithouto <youwithouto.z@gmail.com>
2021-03-14 17:27:25 +1100

    refactor: Optimisation

--- Week14/jsx/main.js
+++ Week14/jsx/main.js
@@ -27,26 +27,34 @@ class Carousel extends Component {
 
         // manual play
         let position = 0;
-        this.root.addEventListener("mousedown", (event) => {
+        this.root.addEventListener("mousedown", event => {
             let children = this.root.children;
             let startX = event.clientX;
 
-            let move = (event) => {
+            let move = event => {
                 let x = event.clientX - startX;
 
-                for (let child of children) {
-                    child.style.transition = "none";
-                    child.style.transform = `translateX(${- position * 500 + x}px)`;
+                let current = position - ((x - x % 500) / 500);
+
+                for (let offset of [-2, -1, 0, 1, 2]) {
+                    let pos = current + offset;
+                    pos = (pos + children.length) % children.length;
+
+                    children[pos].style.transition = "none";
+                    children[pos].style.transform = `translateX(${- pos * 500 + offset * 500 + x % 500}px)`;
                 }
             };
 
-            let up = (event) => {
+            let up = event => {
                 let x = event.clientX - startX;
                 position = position - Math.round(x / 500);
 
-                for (let child of children) {
-                    child.style.transition = "";
-                    child.style.transform = `translateX(${- position * 500}px)`;
+                for (let offset of [0, - Math.sign(Math.round(x / 500) - x + 250 * Math.sign(x))]) {
+                    let pos = position + offset;
+                    pos = (pos + children.length) % children.length;
+
+                    children[pos].style.transition = "";
+                    children[pos].style.transform = `translateX(${- pos * 500 + offset * 500}px)`;
                 }
                 document.removeEventListener("mousemove", move);
                 document.removeEventListener("mouseup", up);
@@ -55,6 +63,7 @@ class Carousel extends Component {
             document.addEventListener("mousemove", move);
             document.addEventListener("mouseup", up);
         });
+
 
         // auto play
         // let currentIndex = 0;
